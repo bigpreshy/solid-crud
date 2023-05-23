5f01ff9574d0c4ae18d7b0bf28d965921971ee9b
Presh <lukeprecious@gmail.com>
2023-05-23 10:59:17 +0100

    send push

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,44 +1,57 @@
-import solidimage from '../assets/images/solid.jpg'
+import { For, createResource } from "solid-js";
+import solidimage from "../assets/images/solid.jpg";
+
+const getBlogs = async () => {
+  const response = await fetch(
+    "https://crudcrud.com/api/6a1613c7646f4a908158592cfdb448aa/blog"
+  );
+  return response.json();
+};
+
 function Blog() {
+  const [blog] = createResource(getBlogs);
   return (
-    <>
-      <div class="card bg-transparent border-0">
-        <div class="row g-3">
-          <div class="col-4">
-            <img class="rounded" src={solidimage} alt="" />
-          </div>
-          <div class="col-8">
-            <a
-              href="#"
-              class="badge bg-danger bg-opacity-10 text-danger mb-2 fw-bold"
-            >
-             Tech
-            </a>
-            <h5>
-              <a
-                href="blog-details.html"
-                class="btn-link stretched-link text-reset fw-bold"
-              >
-                What is Solidjs? 
-              </a>
-            </h5>
-            <div class="d-none d-sm-inline-block">
-              <p class="mb-2">
-              SolidJS is an exciting framework for web development that has been gaining traction in recent years. It has been created by Misko Hevery, the creator of Angular. 
-.
-              </p>
+    <Show when={blog()}>
+      <For each={blog()}>
+        {(eachblog) => (
+          <>
+            <div class="card bg-transparent border-0">
+              <div class="row g-3">
+                <div class="col-4">
+                  <img class="rounded" src={solidimage} alt="" />
+                </div>
+                <div class="col-8">
+                  <a
+                    href="#"
+                    class="badge bg-danger bg-opacity-10 text-danger mb-2 fw-bold"
+                  >
+                    {eachblog.tag}
+                  </a>
+                  <h5>
+                    <a
+                      href="blog-details.html"
+                      class="btn-link stretched-link text-reset fw-bold"
+                    >
+                      {eachblog.title}
+                    </a>
+                  </h5>
+                  <div class="d-none d-sm-inline-block">
+                    <p class="mb-2">{eachblog.text}</p>
 
-              <a class="small text-secondary" href="#!">
-                {" "}
-                <i class="bi bi-calendar-date pe-1"></i> Jan 22, 2022
-              </a>
+                    <a class="small text-secondary" href="#!">
+                      {" "}
+                      <i class="bi bi-calendar-date pe-1"></i> Jan 22, 2022
+                    </a>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </div>
 
-      <hr class="my-4" />
-    </>
+            <hr class="my-4" />
+          </>
+        )}
+      </For>
+    </Show>
   );
 }
 export default Blog;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import  "../assets/css/style.css";
+import "../assets/css/style.css";
 import { type VoidComponent } from "solid-js";
 import { A } from "solid-start";
 import Blog from '../components/Blog'
@@ -6,25 +6,19 @@ import Blog from '../components/Blog'
 const Home: VoidComponent = () => {
   return (
     <main>
-       <div class="container">
-          <div class="row g-4">
+      <div class="container">
+        <div class="row g-4">
+          <div class="col-lg-8">
+            <div class="bg-mode p-4">
+              <h1 class="h4 mb-4">Latest blogs</h1>
+              <Blog />
+            
 
-            <div class="col-lg-8">
-              <div class="bg-mode p-4">
-                <h1 class="h4 mb-4">Latest blogs</h1>
-
-                <Blog/>
-                <Blog/>
-                <Blog/>
-
-
-
-
-                </div>
             </div>
-          
           </div>
+
         </div>
+      </div>
     </main>
   );
 };
